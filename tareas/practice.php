a6c2c640d8ade068d7f7756610fe75d11887f31e
AlejoHProgrammer <129424733+AlejoHProgrammer@users.noreply.github.com>
2023-06-05 15:21:50 -0500

    Update practice.php

--- tareas/practice.php
+++ tareas/practice.php
@@ -1,50 +1,69 @@
 <?php
 
-echo "TAREAS <br>";
-class tarea {
+class tarea
+{
     public $descripcion;
     public $completa;
 
-    function __construct($descripcion, $completa) {
+    function __construct($descripcion, $completa)
+    {
         $this->descripcion = $descripcion;
         $this->completa = $completa;
     }
 
-    function descripcionTarea() {
+    function descripcionTarea()
+    {
         return $this->descripcion;
     }
 
-    function tareaCompleta() {
+    function tareaCompleta()
+    {
         return $this->completa;
     }
-
-    
 }
 
-class listatareas {
+class listatareas
+{
     public $tareas;
 
-    function __construct() {
+    function __construct()
+    {
         $this->tareas = [];
     }
 
-    function aggtarea($descripcion, $completa) {
+    function aggtarea($descripcion, $completa)
+    {
         $tarea = new tarea($descripcion, $completa);
         $this->tareas[] = $tarea;
     }
 
-    function mostrarTareas() {
+    function mostrarTareas()
+    {
+        echo '<table  class="table table-dark table-bordered table-striped">';
+        echo '<tr><th>Tarea</th><th>Completada</th></tr>';
+
         foreach ($this->tareas as $tarea) {
-            echo "Descripción: " . $tarea->descripcionTarea() . ", Completa: " . ($tarea->tareaCompleta() ? "Sí" : "No") . "<br>";
+            echo '<tr>';
+            echo '<td>' . $tarea->descripcionTarea() . '</td>';
+            echo '<td>' . ($tarea->tareaCompleta() ? 'Sí' : 'No') . '</td>';
+            echo '</tr>';
         }
+
+        echo '</table>';
     }
+
+    // function mostrarTareas() {
+    //     foreach ($this->tareas as $tarea) {
+    //         echo "Descripción: " . $tarea->descripcionTarea() . ", Completa: " . ($tarea->tareaCompleta() ? "Sí" : "No") . "<br>";
+    //     }
+    // }
 }
 
 // Ejemplo de uso
 $lista = new listatareas();
 $lista->aggtarea("Descripción de la tarea 1", true);
-$lista->aggtarea("Descripción de la tarea 2", false); 
-$lista->aggtarea("Descripción de la tarea 2",true); 
+$lista->aggtarea("Descripción de la tarea 2", false);
+$lista->aggtarea("Descripción de la tarea 2", true);
 $lista->mostrarTareas();
 
 echo "<br><br>";
